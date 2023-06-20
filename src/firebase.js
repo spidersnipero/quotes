8a813c780019c1e1cb2835cab89ff2d7c99fe5db
AJAY KOTA <96061217+spidersnipero@users.noreply.github.com>
2023-06-20 22:36:46 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,6 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCD280etWIe6H87cR2pg2y5E7Bq_LJ0waA",
-  authDomain: "quotes-a4962.firebaseapp.com",
-  projectId: "quotes-a4962",
-  storageBucket: "quotes-a4962.appspot.com",
-  messagingSenderId: "177171553697",
-  appId: "1:177171553697:web:bc56f6b874f9affc423fe0",
-  measurementId: "G-SCG4BFTNGJ"
 };
 
 
